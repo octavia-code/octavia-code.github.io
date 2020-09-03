fea550d6480736221911f7dbf169de6a2c933b5a
octavia-code <827322286@qq.com>
2020-09-03 10:26:58 +0800

    Site updated: 2020-09-03 10:26:57

---
+++ js/index.js
@@ -0,0 +1,5 @@
+var imgArr = ['img/welcome-purple.jpg','/img/welcome-green.jpg','/img/welcome-darkcyan.jpg','/img/welcome-red.jpg',
+'/img/welcome-double.jpg','/img/welcome-sky.jpg','/img/welcome-sea.jpg','/img/welcome-tram.jpg','/img/welcome-blue.jpg']
+var img = document.getElementById("cover")
+img.src = imgArr[Math.floor((Math.random()*imgArr.length))]
+
