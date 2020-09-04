87d0b65a2c661ab87f97ae3a3d5b21af026f155a
octavia-code <827322286@qq.com>
2020-09-04 09:08:29 +0800

    update

--- themes/diaspora/source/js/index.js
+++ themes/diaspora/source/js/index.js
@@ -1,5 +1,12 @@
 var imgArr = ['img/welcome-purple.jpg','img/welcome-green.jpg','img/welcome-darkcyan.jpg','img/welcome-red.jpg',
 'img/welcome-double.jpg','img/welcome-sky.jpg','img/welcome-sea.jpg','img/welcome-tram.jpg','img/welcome-blue.jpg']
 var img = document.getElementById("cover")
-img.src = imgArr[Math.floor((Math.random()*imgArr.length))]
-img.crossOrigin = 'anonymous'
+var srcString = img.src.toString()
+for (var i=0;i<imgArr.length;i++){
+    if (srcString.includes(imgArr[i])){
+        srcString = srcString.replace(imgArr[i],imgArr[Math.floor(Math.random() *imgArr.length)])
+        break
+    }
+}
+img.src = srcString
+
