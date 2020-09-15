3c4cba1a3dbcfbfcba42b887197efb06ddf2504e
prime <827322286@qq.com>
2020-09-16 01:00:32 +0800

    'add'

--- themes/diaspora/source/js/index.js
+++ themes/diaspora/source/js/index.js
@@ -1,6 +1,7 @@
-var imgArr = ['img/welcome-purple.jpg','img/welcome-green.jpg','img/welcome-darkcyan.jpg','img/welcome-red.jpg',
-'img/welcome-double.jpg','img/welcome-sky.jpg','img/welcome-sea.jpg','img/welcome-tram.jpg','img/welcome-blue.jpg',
-    'img/mhsj.jpg']
+var imgArr = ['img/welcome-sea.jpg','img/welcome-tram.jpg','img/wall-red.jpg','img/wall-white.jpg','img/wall-queen.png',
+'img/wall-yaoyao.jpg','img/wall-blackwhite.jpg','img/wall-celian.jpg','img/wall-huihui.png','img/wall-three.png',
+'img/wall-anime1.png','img/wall-anime2.png','img/wall-anime3.png','img/wall-anime4.png','img/wall-black.jpg',
+'img/wall-bone.jpg','img/wall-warm.jpg','img/wall-sex.jpg']
 var img = document.getElementById("cover")
 var srcString = img.src.toString()
 for (var i=0;i<imgArr.length;i++){
